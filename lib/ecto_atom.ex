f6837186153b550268816bf537e75299d1840e30
Michael Agra <michael.agra@yarilabs.com>
2020-03-19 11:46:08 +0000

    style, embed_as & equal?

--- lib/ecto_atom.ex
+++ lib/ecto_atom.ex
@@ -1,27 +1,49 @@
 defmodule Ecto.Atom do
-
   @moduledoc """
-
-  Custom Type to support `:atom`
-
-  defmodule Post do
-    use Ecto.Schema
-    schema "posts" do
-      field :atom_field, Ecto.Atom
-    end
-  end
+  Custom Ecto type used to store atoms.
   """
 
   @behaviour Ecto.Type
 
+  #
+  # BEHAVIOUR
+  #
+
   def type, do: :string
 
-  def cast(value) when is_atom(value), do: {:ok, value}
+  def cast(value) when is_binary(value) or is_atom(value) do
+    {:ok, atomize(value)}
+  end
+
   def cast(_), do: :error
 
-  def load(value), do: {:ok, String.to_existing_atom(value)}
+  def load(string) when is_binary(string) do
+    {:ok, atomize(string)}
+  end
 
-  def dump(value) when is_atom(value), do: {:ok, Atom.to_string(value)}
+  def dump(atom) when is_atom(atom) do
+    {:ok, stringify(atom)}
+  end
+
   def dump(_), do: :error
 
+  def embed_as(_) do
+    :self
+  end
+
+  def equal?(atom1, atom2) do
+    atom1 == atom2
+  end
+
+  #
+  # PRIVATE
+  #
+
+  defp atomize(nil), do: nil
+  defp atomize(string) when is_binary(string), do: String.to_atom(string)
+  defp atomize(atom) when is_atom(atom), do: atom
+
+  defp stringify(nil), do: nil
+  defp stringify(string) when is_binary(string), do: string
+  defp stringify(atom) when is_atom(atom), do: Atom.to_string(atom)
 end
